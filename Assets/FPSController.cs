0a2449cf5ed9d9de59c916b4ad441953b94d43d1
C-Borchetta <cabman035@gmail.com>
2022-10-19 14:35:13 -0400

    Initial commit

---
+++ Assets/FPSController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSController : MonoBehaviour
+{
+	public float MouseSensitivity;
+	public Transform CamTransform;
+
+	private float camRotation = 0f;
+
+	private void Start()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	private void Update()
+	{
+		float mouseInputY = Input.GetAxis("Mouse Y") * MouseSensitivity;
+		camRotation -= mouseInputY;
+		camRotation = Mathf.Clamp(camRotation, -90f, 90f);
+		CamTransform.localRotation = Quaternion.Euler(new Vector3(camRotation, 0f, 0f));
+
+		float mouseInputX = Input.GetAxis("Mouse X") * MouseSensitivity;
+		transform.rotation = Quaternion.Euler(transform.eulerAngles + new Vector3(0f, mouseInputX, 0f));
+
+		if(Input.GetMouseButtonDown(0))
+		{
+			RaycastHit hit;
+
+			if (Physics.Raycast(CamTransform.position, CamTransform.forward, out hit))
+			{
+				Debug.DrawLine(CamTransform.position + new Vector3(0f, -1f, 0f), hit.point, Color.green, 1f);
+				Debug.Log(hit.collider.gameObject.name);
+			}
+			else
+			{
+				Debug.DrawRay(CamTransform.position + new Vector3(0f, -1f, 0f), CamTransform.forward * 100f, Color.red, 1f);
+			}
+		}
+	}
+}
---
+++ Assets/FPSMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class FPSMovement : MonoBehaviour
+{
+	public CharacterController CC;
+	public float MoveSpeed;
+	public float Gravity = -9.8f;
+	public float JumpSpeed;
+
+	public float verticalSpeed;
+
+	public Light light;
+	public float flickerdelay;
+	public Vector2 intensityRange;
+	public Vector2 delayRange;
+	public float targetIntens;
+
+	private void Update()
+	{
+		flickerdelay -= Time.deltaTime;
+		if(flickerdelay <= 0)
+        {
+			light.intensity = Random.Range(intensityRange.x, intensityRange.y);
+			flickerdelay = Random.Range(delayRange.x, delayRange.y);
+        }
+
+		light.intensity = Mathf.Lerp(light.intensity, targetIntens, Time.deltaTime * 6);
+		
+
+		Vector3 movement = Vector3.zero;
+
+		// X/Z movement
+		float forwardMovement = Input.GetAxis("Vertical") * MoveSpeed * Time.deltaTime;
+		float sideMovement = Input.GetAxis("Horizontal") * MoveSpeed * Time.deltaTime;
+
+		movement += (transform.forward * forwardMovement) + (transform.right * sideMovement);
+
+		if (CC.isGrounded)
+		{
+			verticalSpeed = 0f;
+			if(Input.GetKeyDown(KeyCode.Space))
+			{
+				verticalSpeed = JumpSpeed;
+			}
+		}
+		
+		verticalSpeed += (Gravity * Time.deltaTime);
+		movement += (transform.up * verticalSpeed * Time.deltaTime);
+
+		CC.Move(movement);
+	}
+}
